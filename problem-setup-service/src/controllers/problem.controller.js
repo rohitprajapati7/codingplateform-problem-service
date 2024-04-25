fe926d28314f42a24a95cb97cf0c7ce3ab223136
Ritesh Kshatriya <riteshkshatriya@Riteshs-MacBook-Pro.local>
2024-04-26 01:04:44 +0530

    added api routing structure

---
+++ problem-setup-service/src/controllers/problem.controller.js
@@ -0,0 +1,21 @@
+function pingProblemController (req, res) {
+    res.json({message:'Ping controller in working'})
+}
+
+function addProblem (req, res) {
+
+}
+
+function getProblems (req, res) {
+
+}
+
+function deleteProblem (req, res) {
+
+}
+
+function updateProblem (req, res) {
+
+}
+
+module.exports = {pingProblemController,addProblem,getProblems,deleteProblem,updateProblem}
--- problem-setup-service/src/index.js
+++ problem-setup-service/src/index.js
@@ -3,12 +3,16 @@ const bodyParser = require('body-parser');
 
 
 const {PORT} = require('./config/server.config');
+const apiRouter = require('./routes');
 
 const app = new express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text())
+
+
+app.use('/api', apiRouter);
 
 app.get('/ping',(req, res)=>{
     res.json({message:'Problem service is alive.'})
